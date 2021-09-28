65fedf40fff5b5e97aafecda2ef0d3681f7a8a3c
Rafael <rafaelcacilhas@gmail.com>
2021-09-28 11:05:33 -0300

    versao 1

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
 import HireService from "./HireService/HireService";
+import styled  from 'styled-components';
+
+
+
+const Header = styled.div`
+  background-color: grey;
+  display:          flex;
+  justify-content:  space-around;
+`
+
+const Corpo = styled.div`
+  display:          flex;
+  flex-direction:   column;
+  align-items:      center;
+`
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
+        <Header>
+
+          Ninjas 
+
+          <div>
+            <button>Home</button>
+            <button>Carrinho</button>
+          </div>
+
+        </Header>
+
+        <Corpo>
+          <h1>Labeninjas</h1>
+          <h2>Nosso slogan!</h2>
+
+          {console.log(this.props.servico)}
+
+          <div>
+            <button>Quero ser ninja</button>
+            <button>Contratar um ninja</button>  
+          </div> 
+
+        </Corpo>     
         <HireService />
       </div>
     );
