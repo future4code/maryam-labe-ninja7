2adb3a5f84cf335b04e21a88306d19951155c950
Emilly Santiago <emillysantiago89@gmail.com>
2021-09-29 21:34:57 -0300

    Adicionando pag de detalhe do produto

---
+++ src/components/ServiceDetail/ServiceDetail.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import React from "react";
+import styled from "styled-components";
+import { axiosConfig, baseUrl } from "../../constants";
+
+class ServiceDetail extends React.Component {
+  state = {
+    service: {},
+    paymentMethods: []
+  };
+
+  componentDidMount = () => {
+    this.GetJobById(this.props.serviceID);
+  };
+
+  GetJobById = (id) => {
+
+    axios
+      .get(baseUrl + "/jobs/" + id, axiosConfig)
+      .then((res) => {
+        this.setState({ service: res.data, paymentMethods: res.data.paymentMethods });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    let date = new Date(this.state.service.dueDate);
+    let dateFormat =
+      date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    console.log(this.state.service.paymentMethods);
+
+     const paymentMethod = this.state.paymentMethods.map(
+       (service) => <li key={service}>{service}</li>
+     );
+
+    return (
+      <div>
+        <h3>{this.state.service.title}</h3>
+        <p>Preço: R${this.state.service.price}</p>
+        <p>Prazo: {dateFormat}</p>
+        <p>{this.state.service.description}</p>
+        <div>{paymentMethod}</div>
+        <button
+          onClick={() => {
+            this.props.changePage("services", "");
+          }}
+        >
+          Voltar
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ServiceDetail;
---
+++ src/components/ServicesCard.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+
+class Services extends React.Component {
+  render() {
+    const services = this.props.services.map((service) => {
+      let date = new Date(service.dueDate);
+      let dateFormat =
+        date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+      return (
+        <div>
+          <ProductCardContainer key={service.id}>
+            <h3>{service.title}</h3>
+            <p>Preço: R$ {service.price}</p>
+            <p>Prazo: {dateFormat} </p>
+            <div>
+              <button
+                onClick={() => {
+                  this.props.changePage("seeDetails", service.id);
+                }}
+              >
+                Ver detalhes
+              </button>
+              <button>Adicionar no Carrinho</button>
+            </div>
+          </ProductCardContainer>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <ProductsContainer>{services}</ProductsContainer>
+      </div>
+    );
+  }
+}
+
+export default Services;
+
+const ProductCardContainer = styled.div`
+  width: 300px;
+  height: 250px;
+  background-color: aliceblue;
+  border: 1px solid black;
+  text-align: center;
+  margin: 16px 12px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+`;
+
+const ProductsContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
